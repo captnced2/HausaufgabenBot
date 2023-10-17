5397e4caa3402a0bf3a1453b8c7a499d00e7ea69
captnced <->
2023-10-17 21:25:28 +0200

    Values Files

---
+++ src/main/java/org/values/strings/Console.java
@@ -0,0 +1,201 @@
+package org.values.strings;
+
+import net.dv8tion.jda.api.entities.User;
+import org.config.Config;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.config.Config.writeToLog;
+import static org.time.Time.*;
+import static org.values.Global.*;
+
+public class Console {
+    private static final String startingMessage = "Starting up...";
+    private static final String shutdownMessage = "Shutting down...";
+    private static final String jdaForceShutdownMessage = "Jda not shutting down. Forcing shutdown";
+    private static final String jdaStartupMessage = "Starting Jda...";
+    private static final String jdaStartupCompleteMessage = "Jda startup complete";
+    private static final String cantCreateConfigFileErrorMessage = "Error! Couldn't create config file ";
+    private static final String cantCreateFolderErrorMessage = "Error! Couldn't create config folder ";
+    private static final String creatingNewFileMessage = "Creating new file ";
+    private static final String folderNotFoundMessage = "Creating new folder ";
+    private static final String initCompleteMessage = "Initialization Complete";
+    private static final String calledDayLoopMessage = "Called Day Loop Function";
+    private static final String doneDayLoopMessage = "Finished Day Loop";
+    private static final String postSuccessMessage = "Posted Homework Successfully";
+    private static final String postSkipMessage = "Homework Already Posted. Skipping Message";
+    private static final String delMessageSuccessMessage = "Sent Deletion Messages To Admins";
+    private static final String delMessageSkipMessage = "No Homework To Delete. Skipping Message";
+    private static final String addedHomeworkMessage = "%u added in %s the homework: %h";
+    private static final String setHomeworkMessage = "%u set %s homework to: %h";
+    private static final String requestHomeworkMessage = "%u requested homework for %s";
+    private static final String requestHomeworkTomorrowMessage = "%u requested homework for tomorrow";
+    private static final String postSuccess = "%u used the post command successfully";
+    private static final String delAllMessage = "%u deleted all unchanged homework from today";
+    private static final String delSubjMessage = "%u deleted unchanged homework in %s";
+    private static final String nextDayLoopAt = "Next Day Loop scheduled for ";
+    private static final String commandErrorMessage = "Something went wrong while %u used the %c command";
+    private static final String commandRegisterErrorMessage = "Something went wrong while trying to register %c command";
+    private static final String registeredCommandsMessage = "Successfully registered %i1/%i2 commands";
+    private static final String addedCancelledSubjMessage = "%u added cancelled subject %s on %d";
+    private static final String deletedCommandsMessage = "Deleted %i unused commands";
+
+    public static void sendStartingMessage() {
+        outLog(startingMessage);
+    }
+
+    public static void sendShutdownMessage() {
+        outLog(shutdownMessage);
+    }
+
+    public static void sendJdaForceShutdownMessage() {
+        outLog(jdaForceShutdownMessage);
+    }
+
+    public static void sendCreatingNewFile(String config) {
+        outLog(creatingNewFileMessage + config);
+    }
+
+    public static void sendFolderNotFound(String folder) {
+        outLog(folderNotFoundMessage + folder);
+    }
+
+    public static void sendCantCreateConfError(String config) {
+        outLog(cantCreateConfigFileErrorMessage + config);
+    }
+
+    public static void sendCantCreateFolderError(String folder) {
+        outLog(cantCreateFolderErrorMessage + folder);
+    }
+
+    public static void sendJdaStartup() {
+        out(jdaStartupMessage);
+    }
+
+    public static void sendJdaStartupComplete() {
+        out(jdaStartupCompleteMessage);
+    }
+
+    public static void sendInitComplete() {
+        outLog(initCompleteMessage);
+    }
+
+    public static void sendCalledDayLoopMessage() {
+        outLog(calledDayLoopMessage);
+    }
+
+    public static void sendDoneDayLoopMessage() {
+        outLog(doneDayLoopMessage);
+    }
+
+    public static void sendPostSuccess() {
+        outLog(postSuccessMessage);
+    }
+
+    public static void sendPostSkip() {
+        outLog(postSkipMessage);
+    }
+
+    public static void sendDelMessageSuccess() {
+        outLog(delMessageSuccessMessage);
+    }
+
+    public static void sendDelMessageSkip() {
+        outLog(delMessageSkipMessage);
+    }
+
+    public static void sendAddedHomework(User user, String subjCode, String homework) {
+        outLog(replaceHomework(replaceSubjCode(replaceUser(addedHomeworkMessage, user), subjCode), homework));
+    }
+
+    public static void sendSetHomework(User user, String subjCode, String homework) {
+        outLog(replaceHomework(replaceSubjCode(replaceUser(setHomeworkMessage, user), subjCode), homework));
+    }
+
+    public static void sendRequestedHomework(User user, String subjCode) {
+        outLog(replaceSubjCode(replaceUser(requestHomeworkMessage, user), subjCode));
+    }
+
+    public static void sendRequestedTomorrowHomework(User user) {
+        outLog(replaceUser(requestHomeworkTomorrowMessage, user));
+    }
+
+    public static void sendPostSuccess(User user) {
+        outLog(replaceUser(postSuccess, user));
+    }
+
+    public static void sendDelAll(User user) {
+        outLog(replaceUser(delAllMessage, user));
+    }
+
+    public static void sendDelSubj(User user, String subjCode) {
+        outLog(replaceSubjCode(replaceUser(delSubjMessage, user), subjCode));
+    }
+
+    public static void sendNextDayLoopScheduled(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat(weekdayDayMonthYearHoursMinutesPattern);
+        outLog(nextDayLoopAt + sdf.format(date));
+    }
+
+    public static void sendCommandError(User user, String command) {
+        outLog(replaceCommand(replaceUser(commandErrorMessage, user), command));
+    }
+
+    public static void sendRegisteredCommands(int count, int total) {
+        outLog(replaceIn(replaceIn(registeredCommandsMessage, "%i1", count), "%i2", total));
+    }
+
+    public static void sendCommandRegisterError(String command) {
+        outLog(replaceCommand(commandRegisterErrorMessage, command));
+    }
+
+    public static void sendAddedCancelledSubj(User user, String subjCode, String date) {
+        outLog(replaceDate(replaceSubjCode(replaceUser(addedCancelledSubjMessage, user), subjCode), date));
+    }
+
+    public static void sendDeletedCommands(int count) {
+        outLog(replaceIn(deletedCommandsMessage, "%i", String.valueOf(count)));
+    }
+
+    private static String replaceIn(String string, String regex, String with) {
+        return string.replaceFirst(regex, with);
+    }
+
+    private static String replaceIn(String string, String regex, int with) {
+        return replaceIn(string, regex, String.valueOf(with));
+    }
+
+    private static String replaceUser(String string, User user) {
+        return replaceIn(string, "%u", user.getEffectiveName());
+    }
+
+    private static String replaceSubjCode(String string, String subjCode) {
+        return replaceIn(string, "%s", surroundQuotes(Config.getSubjFromCode(subjCode)));
+    }
+
+    private static String replaceDate(String string, String date) {
+        return replaceIn(string, "%d", date);
+    }
+
+    private static String replaceCommand(String string, String command) {
+        return replaceIn(string, "%c", surroundQuotes(command));
+    }
+
+    private static String replaceHomework(String string, String homework) {
+        return replaceIn(string, "%h", surroundQuotes(homework));
+    }
+
+    private static String surroundQuotes(String in) {
+        return doubleQuotesString + in + doubleQuotesString;
+    }
+
+    private static void outLog(String out) {
+        out(out);
+        writeToLog(getDate() + " " + getCurrentTime() + " : " + out);
+    }
+
+    private static void out(String out) {
+        System.out.println(getDate() + " " + getCurrentTime() + " : " + out);
+    }
+}
---
+++ src/main/java/org/values/strings/Messages.java
@@ -0,0 +1,148 @@
+package org.values.strings;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.awt.*;
+
+import static org.config.Config.getSubjFromCode;
+import static org.time.Time.*;
+import static org.values.Global.*;
+
+public class Messages {
+    public static final String waitingOnLukasMessage = "Wartet auf Luggas...";
+    public static final String LukasIsHere = "Luggas ist da!";
+    public static final String defaultActivity = "Verwaltet Hausaufgaben...";
+    public static final String noHomeworkString = "*Keine Hausaufgaben*";
+    public static final String errorTitle = "Error";
+    public static final String noPermissionTitle = "Keine Rechte";
+    public static final String somethingWentWrongText = "Etwas ist schiefgelaufen";
+    public static final String notAdminText = "Du bist kein Admin User!";
+    public static final String notOwnerText = "Du bist nicht Cedric! >:|";
+    public static final String shutdownTitle = "Shutting down...";
+    public static final String shutdownText = "Good night :D ...";
+    public static final String triggeredDayLoopTitle = "Day loop ausgeführt";
+    public static final String triggeredDayLoopText = "Geht mal wieder nicht...";
+    public static final String notSchooldayText = "Heute ist kein Schultag!";
+    public static final String notSchooldayTomorrow = "Morgen ist keine Schule... Aber hier sind die Hausaufgaben für Montag :D.";
+    public static final String homeworkFromDayTitle = "Hausaufgaben vom ";
+    public static final String homeworkFromDateTitle = " Hausaufgabe vom ";
+    public static final String homeworkToDateTitle = "Hausaufgaben auf ";
+    public static final String addedHomeworkTitle = "Hausaufgabe hinzugefügt";
+    public static final String changedHomeworkTitle = "Hausaufgabe geändert";
+    public static final String noHomeworkFoundTitle = " Hausaufgabe";
+    public static final String noHomeworkFoundText = "*Keine Hausaufgabe*";
+    public static final String wrongServerText = "Falscher Server! Nur auf dem 11a Discord Server verwendbar.";
+    public static final String wrongChannelText = "Falscher Channel! Nur in dem hausaufgaben Channel verwendbar.";
+    public static final String onlyUsableOnWeekdaysText = "Dieser Command ist nur an Wochentagen verwendbar.";
+    public static final String noHomeworkToDeleteText = "Es gibt keine Hausaufgaben zum Löschen.";
+    public static final String deletedAllHomeworkTitle = "Hausaufgaben gelöscht";
+    public static final String deletedHomeworkTitle = "Hausaufgabe gelöscht";
+    public static final String acceptDelHomeworkTitle = "Hausaufgaben zur Löschung freigeben";
+    public static final String addedCancelledSubjTitle = "Fach als entfällt eingetragen";
+
+    private static MessageEmbed makeEmbed(Color color, String title, String text) {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setColor(color);
+        builder.setTitle(title);
+        builder.setDescription(text);
+        builder.setFooter(getWeekday() + embedFooterSeperator + getDate() + embedFooterSeperator + getCurrentTime());
+        return builder.build();
+    }
+
+    private static MessageEmbed successEmbed(String title, String text) {
+        return makeEmbed(successEmbedColor, title, text);
+    }
+
+    private static MessageEmbed errorEmbed(String title, String text) {
+        return makeEmbed(errorEmbedColor, title, text);
+    }
+
+    public static MessageEmbed errorTitleEmbed(String text) {
+        return errorEmbed(errorTitle, text);
+    }
+
+    public static MessageEmbed homeworkEmbed(String title, String text) {
+        return makeEmbed(homeworkEmbedColor, title, text);
+    }
+
+    public static MessageEmbed notAdminEmbed() {
+        return errorEmbed(noPermissionTitle, notAdminText);
+    }
+
+    public static MessageEmbed somethingWentWrongEmbed() {
+        return errorTitleEmbed(somethingWentWrongText);
+    }
+
+    public static MessageEmbed shutdownEmbed() {
+        return successEmbed(shutdownTitle, shutdownText);
+    }
+
+    public static MessageEmbed triggeredDayLoopEmbed() {
+        return successEmbed(triggeredDayLoopTitle, triggeredDayLoopText);
+    }
+
+    public static MessageEmbed notOwnerEmbed() {
+        return errorEmbed(noPermissionTitle, notOwnerText);
+    }
+
+    public static MessageEmbed notSchoolday() {
+        return errorTitleEmbed(notSchooldayText);
+    }
+
+    public static MessageEmbed homeworkFromDay(String homework) {
+        return homeworkEmbed(homeworkFromDayTitle + getWeekday() + " " + getDate(), homework);
+    }
+
+    public static MessageEmbed homeworkToDate(String day, String date, String homework) {
+        return homeworkEmbed(homeworkToDateTitle + day + " " + date, homework);
+    }
+
+    public static MessageEmbed addedHomework(String subjCode, String homework) {
+        return successEmbed(addedHomeworkTitle, getSubjFromCode(subjCode) + subjsRegex + homework);
+    }
+
+    public static MessageEmbed changedHomework(String subjCode, String homework) {
+        return successEmbed(changedHomeworkTitle, getSubjFromCode(subjCode) + subjsRegex + homework);
+    }
+
+    public static MessageEmbed noHomeworkFound(String subjCode) {
+        return homeworkEmbed(getSubjFromCode(subjCode) + noHomeworkFoundTitle, noHomeworkFoundText);
+    }
+
+    public static MessageEmbed homeworkFromDate(String subjCode, String date, String homework) {
+        return homeworkEmbed(getSubjFromCode(subjCode) + homeworkFromDateTitle + date, homework);
+    }
+
+    public static MessageEmbed addedCancelledSubj(String subjCode, String date) {
+        return successEmbed(addedCancelledSubjTitle, date + subjsRegex + getSubjFromCode(subjCode));
+    }
+
+    public static MessageEmbed wrongServer() {
+        return errorTitleEmbed(wrongServerText);
+    }
+
+    public static MessageEmbed wrongChannel() {
+        return errorTitleEmbed(wrongChannelText);
+    }
+
+    public static MessageEmbed onlyUsableOnWeekdays() {
+        return errorTitleEmbed(onlyUsableOnWeekdaysText);
+    }
+
+    public static MessageEmbed noHomeworkToDelete() {
+        return errorTitleEmbed(noHomeworkToDeleteText);
+    }
+
+    public static MessageEmbed deletedAllHomework(String homework) {
+        return successEmbed(deletedAllHomeworkTitle, homework);
+    }
+
+    public static MessageEmbed deletedHomework(String subjCode) {
+        return successEmbed(deletedHomeworkTitle, getSubjFromCode(subjCode));
+    }
+
+    public static MessageEmbed acceptDelHomework(String homework) {
+        return homeworkEmbed(acceptDelHomeworkTitle, homework);
+    }
+}
