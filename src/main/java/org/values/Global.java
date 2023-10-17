fadddb05bd1b65c342d4d1cd54e4b3b16344f55b
captnced <->
2023-10-17 21:56:03 +0200

    Revert "Delete src/main/java/org/values/Global.java"
    This reverts commit 7019c1becaca6844ef247dd136315cb820a9e42e.

---
+++ src/main/java/org/values/Global.java
@@ -0,0 +1,44 @@
+package org.values;
+
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+public class Global {
+    public static final String mainConfigPath = ".\\config\\";
+    public static final String pfpsPath = "pfp";
+    public static final String tokenConfigFileName = "Token.conf";
+    public static final String subjsConfigFileName = "Fach.conf";
+    public static final String adminConfigFileName = "Admins.conf";
+    public static final String homeworkConfigFileName = "Hausaufgaben.conf";
+    public static final String timetableConfigFileName = "Stundenplan.conf";
+    public static final String cancelledSubjectsConfigFileName = "Entfall.conf";
+    public static final String logFileName = "Hausi-Bot.log";
+    public static final String classDiscordServerId = "1055931224859869355";
+    public static final String classDiscordHomeworkChannelId = "1068249752141041717";
+    public static final String classDiscordHomeworkPingRoleId = "1068250503772913784";
+    public static final String classDiscordServerManagerRoleId = "1068208810403041382";
+    public static final String userIdLukas = "771669699830349825";
+    public static final String userIdCedric = "655408730795409409";
+    public static final String subjsRegex = ": ";
+    public static final String commaRegex = ", ";
+    public static final String commentSymbol = "#";
+    public static final Color homeworkEmbedColor = Color.YELLOW;
+    public static final Color errorEmbedColor = Color.RED;
+    public static final Color successEmbedColor = Color.GREEN;
+    public static final String embedFooterSeperator = " • ";
+    public static final String newLine = "\n";
+    public static final String doubleQuotesString = "\"";
+    public static final String commandPackageName = "org.jda.slashcommands.commands";
+    public static final long oneDayInMilliseconds = TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS);
+    public static final String monday = "Montag";
+    public static final String tuesday = "Dienstag";
+    public static final String wednesday = "Mittwoch";
+    public static final String thursday = "Donnerstag";
+    public static final String friday = "Freitag";
+    public static final String saturday = "Samstag";
+    public static final String sunday = "Sonntag";
+    public static final String hoursMinutesPattern = "HH:mm";
+    public static final String dayMonthYearPattern = "dd.MM.yyyy";
+    public static final String weekdayDayMonthYearHoursMinutesPattern = "EEEE dd.MM.yyyy HH:mm";
+    public static final String jpgEnd = ".jpg";
+}
