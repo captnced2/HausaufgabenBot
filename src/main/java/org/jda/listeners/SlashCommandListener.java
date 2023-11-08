af6afdec3773db0a3756cef034ce33e44581dc7d
captnced <->
2023-11-08 19:08:07 +0100

    Few Fixes

--- src/main/java/org/jda/listeners/SlashCommandListener.java
+++ src/main/java/org/jda/listeners/SlashCommandListener.java
@@ -3,7 +3,7 @@ package org.jda.listeners;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jda.JdaMain;
-import org.jda.slashcommands.*;
+import org.jda.slashcommands.JdaSlashCommand;
 
 import static org.jda.JdaMain.*;
 import static org.values.strings.Console.sendError;
@@ -15,8 +15,7 @@ public class SlashCommandListener extends ListenerAdapter {
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         JdaSlashCommand command = JdaMain.getCommandFromName(event.getName());
         if (command != null) {
-            JdaPermission requiredPermission = command.getRequiredPermission();
-            if (hasRequiredPermissions(event.getUser(), requiredPermission)) {
+            if (hasRequiredPermissions(event.getUser(), command.getRequiredPermission())) {
                 try {
                     command.execute(event);
                 } catch (RuntimeException e) {
